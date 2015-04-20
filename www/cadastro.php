13ea91f378648800a5d30e15605f15c5a7d570e2
Hugo <hhldiniz@gmail.com>
2015-04-20 10:43:53 -0300

    Primeiro commit do novo repositorio

---
+++ www/cadastro.php
@@ -0,0 +1,12 @@
+<html>
+    <head><meta charset="utf-8"></head>
+    <body>
+        <?php
+            $nome=$_POST["nome"];
+            $email=$_POST["email"];
+            $usuario=$_POST["usuario"];
+            $senha=$_POST["senha"];
+                
+        ?>
+    </body>
+</html>
