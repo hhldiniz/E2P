14764055dd68917cd0549477c5c344d8fd696dd6
Hugo <hhldiniz@gmail.com>
2015-04-21 20:19:39 -0300

    Inicio validação de form com jquery

---
+++ www/js/validar.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("input").blur(function(){
+     if($(this).val() == "")
+         {
+             $(this).css({"border-color" : "#F00", "padding": "2px"});
+         }
+    });
+})
