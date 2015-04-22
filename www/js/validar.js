b8d0954c8e198056b86e18d417ab150b8b09f60d
Hugo <hhldiniz@gmail.com>
2015-04-22 18:17:37 -0300

    novo arquivo para validação via jquery

--- www/js/validar.js
+++ www/js/validar.js
@@ -2,7 +2,21 @@ $(document).ready(function(){
     $("input").blur(function(){
      if($(this).val() == "")
          {
-             $(this).css({"border-color" : "#F00", "padding": "2px"});
+             $(this).css({"border" : "1px solid #F00", "padding": "2px"});
          }
     });
-})
+    $("#botao").click(function(){
+     var cont = 0;
+     $("#form input").each(function(){
+         if($(this).val() == "")
+             {
+                 $(this).css({"border" : "1px solid #F00", "padding": "2px"});
+                 cont++;
+             }
+        });
+     if(cont == 0)
+         {
+             $("#form").submit();
+         }
+    });
+});
