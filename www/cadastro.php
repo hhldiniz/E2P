476b165e25ae65db261c370376d694f70f7acdca
Hugo <hhldiniz@gmail.com>
2015-04-29 11:36:23 -0300

    ajax encapsulado em arquivo e adicionado tambem funcao ajax no cadastro(ainda bugado)

--- www/cadastro.php
+++ www/cadastro.php
@@ -4,6 +4,9 @@
 	<?php
 		$matricula=$_POST['matricula'];
 	        $senha=$_POST['senha'];
+		$nome=$_POST['nome'];
+		$sobrenome=$_POST['sobrenome'];
+		$email=$_POST['email'];
 		$conexao = mysql_connect("localhost","root","");
            	mysql_select_db("e2p");
 		$sql_string="INSERT INTO usuarios VALUES("."'".$matricula."','".$senha."')";
