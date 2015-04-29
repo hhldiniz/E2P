ce243d9ac6b32e9dc4b70d031ffb0f9415acc792
alysson <alyssonfacanhamoreira@gmail.com>
2015-04-29 09:39:16 -0300

    adicionando php

---
+++ www/login.php
@@ -0,0 +1,24 @@
+<html>
+        <body>
+    <?php
+          $usuario=$_POST['usuario'];
+            $senha=$_POST['senha'];
+          //  $email=$_POST['email'];
+           // $nome=$_POST['nome'];
+            //echo "opa";
+          //  echo("<script>alert("+$nome+")</script>");
+          //  echo($nome);
+            $conexao = mysql_connect("localhost","root","");
+            mysql_select_db("enem");
+           $resultado =  mysql_query("select * from usuarios where usuario='$usuario' and senha='$senha'") or die(mysql_error());
+           // mysql_close($conexao);
+		   if(mysql_num_rows($resultado) > 0)
+		   
+			echo "1";
+			var_dump(http_response_code());
+			else
+			echo "0";
+		   
+        ?>
+        </body>
+</html>
