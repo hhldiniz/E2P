24a3800a715fcc0e37ecdfaa02879433925bac2e
alysson <alyssonfacanhamoreira@gmail.com>
2015-05-07 10:49:13 -0300

    Adicionando confirmar senha

--- www/js/validar.js
+++ www/js/validar.js
@@ -7,7 +7,14 @@ $(document).ready(function(){
     });
     $("#botao").click(function(){
      var cont = 0;
+        
+        if($("#senha2").val() != $("#senha").val()){
+            $("#senha2").css({"border" : "1px solid #F00", "padding": "2px"});
+            cont++;
+        }
+        
      $("#form input").each(function(){
+
          if($(this).val() == "")
              {
                  $(this).css({"border" : "1px solid #F00", "padding": "2px"});
