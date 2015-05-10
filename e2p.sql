4ee26729918ef5960c9f3e41bdf00387fee83803
alysson <alyssonfacanhamoreira@gmail.com>
2015-05-10 12:00:52 -0300

    Atualizando arquivo sql]

--- e2p.sql
+++ e2p.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 06-Maio-2015 às 13:55
--- Versão do servidor: 5.6.21
--- PHP Version: 5.6.3
+-- Host: 127.0.0.1
+-- Generation Time: 10-Maio-2015 às 16:59
+-- Versão do servidor: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,11 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `materias`
+--
+
+CREATE TABLE IF NOT EXISTS `materias` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(205) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `materias`
+--
+
+INSERT INTO `materias` (`id`, `nome`) VALUES
+(1, 'Matemática');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `nivel`
 --
 
 CREATE TABLE IF NOT EXISTS `nivel` (
-`idNivel` int(10) NOT NULL,
+  `idNivel` int(10) NOT NULL,
   `nivel` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `nivel` (
 --
 
 CREATE TABLE IF NOT EXISTS `opcoes` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `content` varchar(255) NOT NULL,
   `right_answer` tinyint(1) NOT NULL,
   `question_id` int(11) NOT NULL
@@ -61,17 +79,18 @@ INSERT INTO `opcoes` (`id`, `content`, `right_answer`, `question_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `questoes` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
-  `idNivel` int(10) NOT NULL
+  `idNivel` int(10) NOT NULL,
+  `id_mate` int(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `questoes`
 --
 
-INSERT INTO `questoes` (`id`, `titulo`, `idNivel`) VALUES
-(1, 'Quanto é dois mais dois?', 0);
+INSERT INTO `questoes` (`id`, `titulo`, `idNivel`, `id_mate`) VALUES
+(1, 'Quanto é dois mais dois?', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -101,48 +120,59 @@ INSERT INTO `usuarios` (`nome`, `sobrenome`, `usuario`, `email`, `matricula`, `s
 --
 
 --
+-- Indexes for table `materias`
+--
+ALTER TABLE `materias`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `nivel`
 --
 ALTER TABLE `nivel`
- ADD PRIMARY KEY (`idNivel`);
+  ADD PRIMARY KEY (`idNivel`);
 
 --
 -- Indexes for table `opcoes`
 --
 ALTER TABLE `opcoes`
- ADD PRIMARY KEY (`id`), ADD KEY `question_id` (`question_id`);
+  ADD PRIMARY KEY (`id`), ADD KEY `question_id` (`question_id`);
 
 --
 -- Indexes for table `questoes`
 --
 ALTER TABLE `questoes`
- ADD PRIMARY KEY (`id`), ADD KEY `idNivel` (`idNivel`);
+  ADD PRIMARY KEY (`id`), ADD KEY `idNivel` (`idNivel`), ADD KEY `id_mate` (`id_mate`), ADD KEY `id_mate_2` (`id_mate`);
 
 --
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
- ADD PRIMARY KEY (`usuario`,`senha`);
+  ADD PRIMARY KEY (`usuario`,`senha`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `materias`
+--
+ALTER TABLE `materias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `nivel`
 --
 ALTER TABLE `nivel`
-MODIFY `idNivel` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idNivel` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `opcoes`
 --
 ALTER TABLE `opcoes`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `questoes`
 --
 ALTER TABLE `questoes`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
