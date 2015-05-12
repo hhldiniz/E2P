fe4ef96f65f0b4503313091ad0b328c6b62cbc3c
Linux <linux@linux-Digitron.(none)>
2015-05-12 15:27:22 -0300

    re-remodelando o banco de dados

--- e2p.sql
+++ e2p.sql
@@ -1,119 +1,159 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (i686)
 --
--- Host: localhost
--- Generation Time: 12-Maio-2015 às 16:56
--- Versão do servidor: 5.6.21
--- PHP Version: 5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: e2p
+-- ------------------------------------------------------
+-- Server version	5.5.43-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `e2p`
+-- Table structure for table `materias`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `materias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `materias` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(205) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `materias`
+-- Dumping data for table `materias`
 --
 
-CREATE TABLE IF NOT EXISTS `materias` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(205) NOT NULL
+LOCK TABLES `materias` WRITE;
+/*!40000 ALTER TABLE `materias` DISABLE KEYS */;
+INSERT INTO `materias` VALUES (1,'Matématica');
+/*!40000 ALTER TABLE `materias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nivel`
+--
+
+DROP TABLE IF EXISTS `nivel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nivel` (
+  `idNivel` int(10) NOT NULL AUTO_INCREMENT,
+  `nivel` int(5) NOT NULL,
+  PRIMARY KEY (`idNivel`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `materias`
+-- Dumping data for table `nivel`
 --
 
-INSERT INTO `materias` (`id`, `nome`) VALUES
-(1, 'Matemática');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `questoes`
---
-
-CREATE TABLE IF NOT EXISTS `questoes` (
-`id` int(11) NOT NULL,
-  `questao_texto` text NOT NULL,
-  `nivel` varchar(10) NOT NULL,
-  `alternativa1` tinyint(1) NOT NULL,
-  `alternativa2` tinyint(1) NOT NULL,
-  `alternativa3` tinyint(1) NOT NULL,
-  `alternativa4` tinyint(1) NOT NULL,
-  `alternativa5` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+LOCK TABLES `nivel` WRITE;
+/*!40000 ALTER TABLE `nivel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nivel` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Extraindo dados da tabela `questoes`
+-- Table structure for table `opcoes`
 --
 
-INSERT INTO `questoes` (`id`, `questao_texto`, `nivel`, `alternativa1`, `alternativa2`, `alternativa3`, `alternativa4`, `alternativa5`) VALUES
-(1, '', '', 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `opcoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `opcoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) NOT NULL,
+  `right_answer` tinyint(1) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `opcoes_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questoes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `usuarios`
+-- Dumping data for table `opcoes`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
+LOCK TABLES `opcoes` WRITE;
+/*!40000 ALTER TABLE `opcoes` DISABLE KEYS */;
+INSERT INTO `opcoes` VALUES (1,'5',0,1),(2,'4',1,1),(3,'3',0,1),(4,'2',0,1),(5,'0',0,2),(6,'1',0,2),(7,'2',1,2),(8,'3',0,2),(9,'9',0,3),(10,'10',1,3),(11,'11',0,3),(12,'12',0,3);
+/*!40000 ALTER TABLE `opcoes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questoes`
+--
+
+DROP TABLE IF EXISTS `questoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `idNivel` int(10) NOT NULL,
+  `id_materia` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idNivel` (`idNivel`),
+  KEY `id_materia` (`id_materia`),
+  CONSTRAINT `questoes_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materias` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questoes`
+--
+
+LOCK TABLES `questoes` WRITE;
+/*!40000 ALTER TABLE `questoes` DISABLE KEYS */;
+INSERT INTO `questoes` VALUES (1,'Quanto é dois mais dois?',0,1),(2,'X + 2 = 4. Qual o valor de X?',0,1),(3,'5 + 5 = ?',0,1);
+/*!40000 ALTER TABLE `questoes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
   `nome` varchar(50) NOT NULL,
   `sobrenome` varchar(50) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `matricula` varchar(50) NOT NULL,
-  `senha` varchar(50) NOT NULL
+  `senha` varchar(50) NOT NULL,
+  PRIMARY KEY (`usuario`,`senha`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Extraindo dados da tabela `usuarios`
+-- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`nome`, `sobrenome`, `usuario`, `email`, `matricula`, `senha`) VALUES
-('', '', '', '', '', ''),
-('', '', '', '', '', 'asd'),
-('', '', '', '', '', 'ssa'),
-('abc', 'abc', 'abc', 'abc@abc.com', '123', '123'),
-('Hugo', 'Diniz', 'hhldiniz', 'hhldiniz@gmail.com', '123', '123'),
-('123', '123', 'hugo', 'diniz', 'aaa', 'aaa@aaa.com');
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES ('abc','abc','abc','abc@abc.com','123','123'),('Alysson','Moreira','alyssonmoreira','alysson@email.com','123456','senha'),('123','123','hugo','diniz','aaa','aaa@aaa.com');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `questoes`
---
-ALTER TABLE `questoes`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
- ADD PRIMARY KEY (`usuario`,`senha`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `questoes`
---
-ALTER TABLE `questoes`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-12 15:25:32
