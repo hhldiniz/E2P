7202d20fd0341b17677960edc50201d1fcfe28b4
alysson <alyssonfacanhamoreira@gmail.com>
2015-05-17 23:16:25 -0300

    Protegendo login.php de sql injection

--- www/login.php
+++ www/login.php
@@ -1,8 +1,12 @@
 <html>
         <body>
     <?php
-          $usuario=$_POST['usuario'];
+            $usuario=$_POST['usuario'];
             $senha=$_POST['senha'];
+
+            $usuario = preg_replace('/[^[:alnum:]_]/', '',$usuario);
+            $senha = preg_replace('/[^[:alnum:]_]/', '',$senha);
+
             $conexao = mysql_connect("localhost","root","");
             mysql_select_db("e2p");
            $resultado =  mysql_query("select * from usuarios where usuario='$usuario' and senha='$senha'") or die(mysql_error());
