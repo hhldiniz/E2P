5b5d8426708fd25c4429e31af6f1eaeedd460df9
Hugo <hhldiniz@gmail.com>
2015-05-18 10:10:30 -0300

    Adicionado novos campos no cadastro de questões e php de questões modificado

--- www/cadastroQuestoes.php
+++ www/cadastroQuestoes.php
@@ -2,14 +2,13 @@
 <body>
 	<?php
 		$questao_texto=$_GET['questao_texto'];
-		$alternativa1=$_GET['alternativa1'];
-		$alternativa2=$_GET['alternativa2'];
-		$alternativa3=$_GET['alternativa3'];
-		$alternativa4=$_GET['alternativa4'];
-		$alternativa5=$_GET['alternativa5'];
+		$alternativa=$_GET['alternativa'];
+		$nivel=$_GET['nivel'];
+		$materia=$_GET['materia'];
 		$conexao=mysql_connect("localhost","root","");
 		mysql_select_db("e2p");
-		$resultado=mysql_query("INSERT INTO questoes VALUES"."'".$questao_texto."','".$alternativa1."','".$alternativa2."','".$alternativa3."','".$alternativa4."','".$alternativa5."'");
+		mysql_query("INSERT INTO questoes VALUES"."'".$questao_texto."','".$nivel."','".$materia."'");
+		mysql_close($conexao);
 	?>
 </body>
 </head>
