8ad6aa7a6e7452becc9f196e11e56323b7b0ee50
Linux <linux@linux-Digitron.(none)>
2015-05-22 16:47:30 -0300

    Começo da logica de acertos por materias

--- www/php/enviarQuestoesAcertos.php
+++ www/php/enviarQuestoesAcertos.php
@@ -10,8 +10,22 @@
     $acertosmat = json_decode($_POST['acertosmat']);
 
 
-    $sql = "INSERT INTO estatisticas (acertos_geral,total_questoes";
+    $sql = "UPDATE estatisticas SET acertos_geral = acertos_geral+".$totalac."";
+    $sql .= ",total_questoes = total_questoes + ".$totalq."";
 
+    $sql .= ",total_mat = total_mat + ".$totalqmat->mat."";
+    $sql .= ",total_geo = total_geo + ".$totalqmat->geo."";
+    $sql .= ",total_hist = total_hist + ".$totalqmat->hist."";
+    $sql .= ",total_port = total_port + ".$totalqmat->port."";
+    $sql .= ",total_quim = total_quim + ".$totalqmat->quim."";
+
+    $sql .= ",acertos_mat = acertos_mat + ".$acertosmat->mat."";
+    $sql .= ",acertos_geo = acertos_geo + ".$acertosmat->geo."";
+    $sql .= ",acertos_hist = acertos_hist + ".$acertosmat->hist."";
+    $sql .= ",acertos_port = acertos_port + ".$acertosmat->port."";
+    $sql .= ",acertos_quim = acertos_quim + ".$acertosmat->quim."";
+
+    $sql .= "WHERE user=".$usuario."";
 
 	mysql_query();
 	mysql_close($conexao);
