c320a10693d64cc03879caa64de22e3ba279386c
Linux <linux@linux-Digitron.(none)>
2015-06-02 16:12:45 -0300

    grafico de estatisticas inicial

--- www/js/controleDeGrafico.js
+++ www/js/controleDeGrafico.js
@@ -1,53 +1,68 @@
-function requestData() {
-    $.ajax({
-        url: 'php/ranking.php',
-        success: function(point) {
-            var series = chart.series[0],
-                shift = series.data.length > 20; // shift if the series is 
-                                                 // longer than 20
 
-            // add the point
-            chart.series[0].addPoint(point, true, shift);
+	var est = new XMLHttpRequest();
+        est.open("POST","php/estatisticas.php",true);
+        est.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     
+     var vars = "user="+localStorage.usuario;
+
+     est.onreadystatechange = function() {
+	    if(est.readyState == 4 && est.status == 200) {
             
-            // call it again after one second
-            setTimeout(requestData, 1000);    
-        },
-        cache: false
-    });
-}
-$(document).ready(function(){
- chart = new Highcharts.Chart({
-chart:
-{
-	renderTo: grafico,
-	type: 'line', 
-	events:
-	{
-		load: requestData
-	}
-},
-title:
-{ 
-	text:'Seu desempenho'
-},
-xAxix:
-{
-	title:
-		{
-			text: 'Tempo'
-		}	
-},
 
-yAxix:
-{	
-	title:
-		{
-			text: 'Acertos'
-		}
-},
-series:[{
-	name: "Teste",
-	data: [],
-}]
-});
-});
+};
+		    var return_data = est.responseText;
+            return_data = $.parseJSON(return_data);
+           // window.location.href = "home.html";
+           var total = Number(return_data[0].acertos_geral);
+
+           $('#grafico').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Aproveitamento por matérias'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Porcentagem',
+            data: [
+            ["Física", (Number(return_data[0].acertos_fis)/total)*100],
+            ["Matematica", (Number(return_data[0].acertos_mat)/total)*100],
+            ["Geografia", (Number(return_data[0].acertos_geo)/total)*100],
+          	["História", (Number(return_data[0].acertos_hist)/total)*100],
+          	["Português", (Number(return_data[0].acertos_port)/total)*100],
+          	["Química", (Number(return_data[0].acertos_quim)/total)*100],
+            ["Biologia", (Number(return_data[0].acertos_bio)/total)*100]
+            ]
+        }]
+    });
+
+
+         
+
+	   // chart1 = new Highcharts.Chart(chartOptions);
+    
+    }
+    // Send the data to PHP now... and wait for response to update the status div
+    est.send(vars); // Actually execute the request
+    //$("#status").html("processing...");	
+      
+
