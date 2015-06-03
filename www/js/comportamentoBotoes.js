983a64a450ea7cb7a132aef1de37a08ab4f984e3
Hugo <hhldiniz@gmail.com>
2015-06-03 14:13:17 -0300

    erro

--- www/js/comportamentoBotoes.js
+++ www/js/comportamentoBotoes.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
 		localStorage.setItem("usuario",null);
 		location.href="http://localhost/E2P/www/";
 });
-	$(".icone_menu joystick").click(function(){
+	$("#joystick").click(function(){
 		location.href="http://localhost/E2P/www/html/partida.html";
 });
-	$(".icone_menu estrela").click(function(){
+	$("#estrela").click(function(){
 		location.href="http://localhost/E2P/www/html/insignias.html";
 });
-	$(".icone_menu estatistica").click(function(){
+	$("#estatistica").click(function(){
 		location.href="http://localhost/E2P/www/html/estatisticas.html";
 });
-	$(".icone_menu coroa").click(function()({
+	$("#coroa").click(function(){
 		location.href="http://localhost/E2P/www/html/ranking.html";
 });
 });
