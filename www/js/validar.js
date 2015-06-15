85dc65a8a84d6309b22e2df375d4ce7af67862cf
Hugo <hhldiniz@gmail.com>
2015-06-15 11:54:09 -0300

    Pequenas alterações

--- www/js/validar.js
+++ www/js/validar.js
@@ -1,23 +1,26 @@
 $(document).ready(function(){ 
-     $("html").attr("lang","pt");
+     $("html").attr("lang","pt"); //define a linguagem trabalhada no html
+     $("head").append("<script src='jquery.validate.min.js'></script>"); //plugin adicionado posteriormente, para polpar trabalho, está sendo adicionado via código
+     $("input").attr("spellcheck","true");//habilita spellcheck para input
+     $("textarea").attr("spellcheck","true");//habilita spellcheck para textarea
      $("input").blur(function(){ //adiciona borda vermelha quando campo é deixado em branco
      if($(this).val() == "")
          {
-             $(this).css({"border" : "1px solid #F00", "padding": "2px"});
+             $(this).css({"border" : "1px solid #F00", "padding": "2px"}); //altera a cor ao redor do campo
          }
 	else
-		$(this).css({"border": "1px solid #888"});
+		$(this).css({"border": "1px solid #888"}); //tb altera a cor caso aconteça o contrário da condição acima
     });
-    $("input").click(function(){
-	$(this).attr("spellcheck","true");
-});
-    $("textarea").attr("spellcheck","true");
     $("textarea").blur(function(){
+	$(this).validate({
+	rules:{
+	
+}
+});
 	if($(this).val()=="")
 		$(this).css({"border" : "1px solid #F00", "padding": "2px"});
 	else
-		$(this).css({"border": "1px solid #888"});
-	
+		$(this).css({"border": "1px solid #888"});	
 });
     $("#botao_cadastrar").click(function(){ //usando expressão regular para validar o email pois o html5 está deixando passar
 	var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
