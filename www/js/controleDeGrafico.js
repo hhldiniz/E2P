4427d5e82fc8cf9978f8f0d2f88ee25e1132239f
Linux <linux@linux-Digitron.(none)>
2015-06-19 16:45:33 -0300

    Concertando grafico de estatisticas e adicionando top 10

--- www/js/controleDeGrafico.js
+++ www/js/controleDeGrafico.js
@@ -1,5 +1,6 @@
 
-	var est = new XMLHttpRequest();
+	function estat(){
+    var est = new XMLHttpRequest();
         est.open("POST","php/estatisticas.php",true);
         est.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
      
@@ -28,21 +29,18 @@
             text: 'Aproveitamento por matérias'
         },
         tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
                 }
-            }
-        },
+            },
         series: [{
             type: 'pie',
             name: 'Porcentagem',
@@ -74,3 +72,91 @@
     //$("#status").html("processing...");	
       
 
+}
+
+
+function ranking(){
+    var est = new XMLHttpRequest();
+        est.open("POST","php/ranking.php",true);
+        est.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     
+    
+
+     est.onreadystatechange = function() {
+        if(est.readyState == 4 && est.status == 200) {
+            
+
+};
+            var return_data = est.responseText;
+            return_data = $.parseJSON(return_data);
+           // window.location.href = "home.html";
+          // var total = Number(return_data[0].acertos_geral);
+
+           
+
+
+           $('#grafico').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Top 10 Aprendizes'
+        },
+        xAxis: {
+            categories: [
+                return_data[0].user,
+                return_data[1].user,
+                return_data[2].user,
+                return_data[3].user
+                /*return_data[4].user,
+                return_data[5].user,
+                return_data[6].user,
+                return_data[7].user,
+                return_data[8].user,
+                return_data[9].user*/
+            ]
+        },
+        yAxis: [{
+            min: 0,
+            title: {
+                text: 'Acertos'
+            }
+        } ],
+        legend: {
+            shadow: false
+        },
+        tooltip: {
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                grouping: false,
+                shadow: false,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Acertos',
+
+            color: 'rgba(24,143,177,1)',
+            data: [Number(return_data[0].acertos_geral), Number(return_data[1].acertos_geral), Number(return_data[2].acertos_geral),Number(return_data[3].acertos_geral)],
+            pointPadding: 0.3,
+            pointPlacement: 0
+        }, 
+              
+            
+       ]
+    });
+
+
+         
+
+       // chart1 = new Highcharts.Chart(chartOptions);
+    
+    }
+    // Send the data to PHP now... and wait for response to update the status div
+    est.send(); // Actually execute the request
+    //$("#status").html("processing...");   
+      
+
+}
