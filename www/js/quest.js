8a64cfd1795cd5f6473a96b70cab442de27d2055
alysson <alyssonfacanhamoreira@gmail.com>
2015-06-20 01:45:18 -0300

    Passando de nivel ao final da partida e avisando ao jogador

--- www/js/quest.js
+++ www/js/quest.js
@@ -213,6 +213,7 @@ function enviarQuestoesAcertos(esta,estaacertos,numquest,acertos)
             
             var return_data = hr.responseText;
             estatistics();
+            checaNivel();
             checaAchievements();
            // window.location.href = "home.html";
             
@@ -431,6 +432,69 @@ function checaQuestao() {
         }
     });
 }
+    function checaNivel(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST","php/professor.php",true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        var vars = "user="+localStorage.usuario;
+    
+        xhr.onreadystatechange = function(){
+	    if(xhr.readyState == 4 && xhr.status == 200){
+		    var return_data = xhr.responseText;
+            return_data = $.parseJSON(return_data);
+            //$("#prg").progressbar({value: return_data.acertos_geral});
+            //$("#prg").val(Number(return_data[0].acertos_geral));
+            //$("#prg").max(((Number(return_data[0].nivel)+1)*10)-10);
+
+            if(Number(return_data[0].acertos_geral) >= ((Number(return_data[0].nivel)+1)*15)-15){
+               levelUpP(localStorage.usuario);
+            }else{
+            
+          ///  prg.max = ((Number(return_data[0].nivel)+1)*15)-15;
+           // prg.value = Number(return_data[0].acertos_geral);
+            
+            var esch = "Nivel "+return_data[0].nivel+" - "+return_data[0].acertos_geral+" / "+(((Number(return_data[0].nivel)+1)*15)-15);
+            
+            $("#linha_dados_usuario").append(esch);
+            
+			//$("#fraseTeacher").html(return_data[0].content); //gambiarra para contornar problema indescritivel com javascript
+        }
+	    }
+    }
+    // Send the data to PHP now... and wait for response to update the status div
+    xhr.send(vars); // Actually execute the request
+    
+    }
+    
+    function levelUpP(user){
+        
+        
+        lvl = new XMLHttpRequest(); 
+
+        lvl.open("POST","php/professor.php",true);
+        lvl.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        var vars = "usuario="+user;
+    
+        lvl.onreadystatechange = function(){
+        if(lvl.readyState == 4 && lvl.status == 200){
+            if(Number(lvl.responseText)>0){
+            alert("Muito bem!");}
+            //var return_data = lvl.responseText;
+            //return_data = $.parseJSON(return_data);
+            //console.log(return_data[0].nivel);
+            
+            //$("#linha_dados_usuario").append("Nivel "+return_data[0].nivel);
+            
+            //$("#linha_dados_usuario").append(esch);
+            
+            //$("#fraseTeacher").html(return_data[0].content); //gambiarra para contornar problema indescritivel com javascript
+        }
+        }
+
+         lvl.send(vars);
+    }
 
 $("#confirm").click(function(event) {
     /* Act on the event */
