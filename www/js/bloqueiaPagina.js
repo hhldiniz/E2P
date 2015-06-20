6173ed469d1b665981a62e35c4048166c7d1c400
Hugo <hhldiniz@gmail.com>
2015-06-20 14:17:44 -0300

    Script para bloquear usuario após 30min foi inserido

--- www/js/bloqueiaPagina.js
+++ www/js/bloqueiaPagina.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	if(localStorage.getItem("usuario")==null)
+	if(localStorage.getItem("usuario")==null && $.now()-localStorage.getItem("hora_login")>=1800000) //numero equivale a 30min em milisegundos
 	{
 		$(document.window.href).attr("href","bloqueio.html");
 	}
