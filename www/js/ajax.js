8eb035c38f39aa15f14cc974006aa663e84523fb
Hugo <hhldiniz@gmail.com>
2015-06-20 19:08:28 -0300

    Cadastro de questão concluido mas ainda precisa ser testado. Validação para escolhas de resposta correta durante o cadastro inserido

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -111,11 +111,12 @@ function ajax_cadastro() //função ajax para cadastro do usuário
 function ajaxCadastraQuestao() //função para cadastro da questão
 {
 	 httpRequestCreate("GET","php/cadastroQuestoes.php");
+	var questao_titulo=$("#questao_titulo").val();
 	var questao_texto=$("#questao_texto").val();
 	var alternativa=$("#alternativa").val();
 	var nivel=$("#nivel").val();
 	var materia=$("#materia").val();
-	var vars="questao_texto="+questao_texto+"&alternativa="+alternativa+"&nivel="+nivel+"&materia="+materia;
+	var vars="questao_titulo="+questao_titulo+"&questao_texto="+questao_texto+"&alternativa="+alternativa+"&materia="+materia+"&nivel="+nivel;
     // Access the onreadystatechange event for the XMLHttpRequest object
     hr.onreadystatechange = function() {
 	    if(hr.readyState == 4 && hr.status == 200) {
--- www/php/cadastroQuestoes.php
+++ www/php/cadastroQuestoes.php
@@ -1,15 +1,15 @@
 <html>
 <body>
 	<?php
-		$questao_titulo=$_GET['questao_titulos'];
+		$questao_titulo=$_GET['questao_titulo'];
 		$questao_texto=$_GET['questao_texto'];
 		$alternativa=$_GET['alternativa'];
 		$materia=$_GET['materia'];
 		$nivel=$_GET['nivel'];
 		$conexao=mysql_connect("localhost","root","");
 		mysql_select_db("e2p");
-		$resultado=mysql_query("INSERT INTO questoes VALUES"."'".$questao_texto."','".$nivel."','".$materia."'");
-		$resultado2=mysql_query("INSERT INTO opcoes VALUES"."'".);
+		$resultado=mysql_query("INSERT INTO questoes VALUES"."'".$questao_titulo."','".$nivel."','".$materia."'");
+		$resultado2=mysql_query("INSERT INTO opcoes VALUES"."'".$questao_texto."','".$alternativa."','LAST_INSERT_ID()'");
 		if($resultado && $resultado2)
 		{
 			echo("1");
