cce7f78785cd005356205b0853c4ce299a18cc85
alysson <alyssonfacanhamoreira@gmail.com>
2015-06-21 13:51:59 -0300

    resolvendo problema no cadastro de questoes

--- www/php/cadastroQuestoes.php
+++ www/php/cadastroQuestoes.php
@@ -29,13 +29,14 @@
        
 		if($resultado && $resultado2)
 		{
-            echo($sql2);
-			//echo("1");
+            //echo($sql2);
+            //echo(mysql_error());
+			echo("1");
 		}
 		else
 		{
-            echo($sql2);
-			//echo("0");
+           // echo($sql2);
+			echo("0");
 		}
 		mysql_close($conexao);
 	?>
