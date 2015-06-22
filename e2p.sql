fc94a2bceef51481c670923b9c2c7141207a6df0
alysson <alyssonfacanhamoreira@gmail.com>
2015-06-21 21:04:09 -0300

    adicionando campo partidas realizadas na tabela estatisticas

--- e2p.sql
+++ e2p.sql
@@ -1,32 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tempo de Gera√ß√£o: 03/06/2015 √†s 15h48min
--- Vers√£o do Servidor: 5.5.43
--- Vers√£o do PHP: 5.3.10-1ubuntu3.18
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+# SQL Manager 2005 Lite for MySQL 3.7.7.1
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : e2p
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET FOREIGN_KEY_CHECKS=0;
 
---
--- Banco de Dados: `e2p`
---
+DROP DATABASE IF EXISTS `e2p`;
 
--- --------------------------------------------------------
+CREATE DATABASE `e2p`
+    CHARACTER SET 'utf8'
+    COLLATE 'utf8_general_ci';
 
---
--- Estrutura da tabela `estatisticas`
---
+USE `e2p`;
 
-CREATE TABLE IF NOT EXISTS `estatisticas` (
+#
+# Structure for the `estatisticas` table : 
+#
+
+CREATE TABLE `estatisticas` (
   `user` varchar(205) NOT NULL,
   `acertos_geral` int(11) NOT NULL DEFAULT '0',
   `total_questoes` int(11) NOT NULL DEFAULT '0',
@@ -51,168 +44,46 @@ CREATE TABLE IF NOT EXISTS `estatisticas` (
   `total_fis` int(11) NOT NULL DEFAULT '0',
   `acertos_fis` int(11) NOT NULL DEFAULT '0',
   `total_bio` int(11) NOT NULL DEFAULT '0',
-  `acertos_bio` int(11) NOT NULL DEFAULT '0'
+  `acertos_bio` int(11) NOT NULL DEFAULT '0',
+  `jogos_realizados` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `estatisticas`
---
+#
+# Structure for the `frases` table : 
+#
 
-INSERT INTO `estatisticas` (`user`, `acertos_geral`, `total_questoes`, `total_mat`, `acertos_mat`, `total_geo`, `acertos_geo`, `total_hist`, `acertos_hist`, `total_port`, `acertos_port`, `total_quim`, `acertos_quim`, `total_ing`, `acertos_ing`, `total_esp`, `acertos_esp`, `total_soc`, `acertos_soc`, `total_fil`, `acertos_fil`, `total_fis`, `acertos_fis`, `total_bio`, `acertos_bio`) VALUES
-('alyssonmoreira', 105, 105, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('asdf', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('alicetotosa', 16, 20, 7, 5, 1, 1, 4, 3, 7, 6, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('ozeias', 140, 143, 52, 51, 5, 5, 40, 39, 28, 28, 18, 17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `frases`
---
-
-CREATE TABLE IF NOT EXISTS `frases` (
+CREATE TABLE `frases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(205) NOT NULL,
   `reacao` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `frases`
---
+#
+# Structure for the `materias` table : 
+#
 
-INSERT INTO `frases` (`id`, `content`, `reacao`) VALUES
-(1, 'Aah!! que √≥timo simula... err digo plataforma!', 1),
-(2, 'Isto n√£o √© um simulador, viu?', 2),
-(3, 'Se voc√™ ver um animal, desconfie se n√£o √© um alien√≠gena!', 3),
-(4, 'Vortex, ahh... N√Éo!!', 4),
-(5, 'Aah!! que √≥timo simula... err digo plataforma!', 5),
-(6, 'Isto n√£o √© um simulador, viu?', 1),
-(7, 'Se voc√™ ver um animal, desconfie se n√£o √© um alien√≠gena!', 2),
-(8, 'Vortex, ahh... N√Éo!!', 3);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `materias`
---
-
-CREATE TABLE IF NOT EXISTS `materias` (
+CREATE TABLE `materias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(205) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `materias`
---
+#
+# Structure for the `nivel` table : 
+#
 
-INSERT INTO `materias` (`id`, `nome`) VALUES
-(1, 'Matem√°tica'),
-(2, 'Geografia'),
-(3, 'Hist√≥ria'),
-(4, 'Portugu√™s'),
-(5, 'Qu√≠mica'),
-(6, 'Ingl√™s'),
-(7, 'Espanhol'),
-(9, 'Sociologia'),
-(10, 'Filosofia'),
-(11, 'F√≠sica'),
-(12, 'Biologia');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `nivel`
---
-
-CREATE TABLE IF NOT EXISTS `nivel` (
+CREATE TABLE `nivel` (
   `idNivel` int(10) NOT NULL AUTO_INCREMENT,
   `nivel` varchar(205) NOT NULL,
   PRIMARY KEY (`idNivel`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `nivel`
---
+#
+# Structure for the `questoes` table : 
+#
 
-INSERT INTO `nivel` (`idNivel`, `nivel`) VALUES
-(1, 'F√°cil'),
-(2, 'M√©dio'),
-(3, 'Dif√≠cil');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `opcoes`
---
-
-CREATE TABLE IF NOT EXISTS `opcoes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(255) NOT NULL,
-  `right_answer` tinyint(1) NOT NULL,
-  `question_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `question_id` (`question_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=47 ;
-
---
--- Extraindo dados da tabela `opcoes`
---
-
-INSERT INTO `opcoes` (`id`, `content`, `right_answer`, `question_id`) VALUES
-(1, '5', 0, 1),
-(2, '4', 1, 1),
-(3, '3', 0, 1),
-(4, '2', 0, 1),
-(5, '1', 0, 2),
-(6, '2', 1, 2),
-(7, '3', 0, 2),
-(8, '4', 0, 2),
-(9, '8', 0, 3),
-(10, '9', 0, 3),
-(11, '10', 1, 3),
-(12, '11', 0, 3),
-(13, 'Enforcado', 1, 4),
-(14, 'Com um tiro no cocurutu', 0, 4),
-(15, 'Negaram √°gua pra ele', 0, 4),
-(16, 'Ele se cansou de viver', 0, 4),
-(17, 'Cear√°', 0, 5),
-(18, 'Maranh√£o', 0, 5),
-(19, 'Pernambuco', 0, 5),
-(20, 'Bahia', 0, 5),
-(21, 'Quebec', 1, 5),
-(22, 'Enorme', 0, 6),
-(23, 'Razoavelmente Importante', 0, 6),
-(24, 'Importante', 0, 6),
-(25, 'N√£o t√£o Importante', 0, 6),
-(26, 'Irrelevante', 1, 6),
-(29, '1', 1, 7),
-(30, '2', 0, 7),
-(31, '3', 0, 7),
-(32, '4', 0, 7),
-(33, '1', 0, 8),
-(34, '2', 1, 8),
-(35, '3', 0, 8),
-(36, '4', 0, 8),
-(37, 'bral', 1, 9),
-(38, 'brel', 0, 9),
-(39, 'bril', 0, 9),
-(40, 'brol', 0, 9),
-(41, 'brul', 0, 9),
-(42, 'Composto', 0, 10),
-(43, 'Com posto', 1, 10),
-(44, 'Simples', 0, 10),
-(45, 'N√£o sei', 0, 10),
-(46, 'Pergunta la no Posto Ipiranga', 0, 10);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `questoes`
---
-
-CREATE TABLE IF NOT EXISTS `questoes` (
+CREATE TABLE `questoes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(255) NOT NULL,
   `idNivel` int(10) NOT NULL,
@@ -222,31 +93,27 @@ CREATE TABLE IF NOT EXISTS `questoes` (
   KEY `id_mate` (`id_mate`),
   KEY `id_mate_2` (`id_mate`),
   KEY `idNivel_2` (`idNivel`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `questoes`
---
+#
+# Structure for the `opcoes` table : 
+#
 
-INSERT INTO `questoes` (`id`, `titulo`, `idNivel`, `id_mate`) VALUES
-(1, 'Quanto √© dois mais dois?', 1, 1),
-(2, 'X + 2 = 4. Qual o valor de X? ', 3, 1),
-(3, '5+5 = ?', 2, 1),
-(4, 'Como Tiradentes foi morto?', 1, 3),
-(5, 'Qual desses estados n√£o faz parte do nordeste brasileiro?', 2, 2),
-(6, 'Qual o grau de relev√¢ncia de ora√ß√£o subordinada para a sua vida?', 1, 4),
-(7, 'Qual o n√∫mero at√¥mico do Hidrog√™nio?', 2, 5),
-(8, 'Qual o n√∫mero at√¥mico do H√©lio?', 2, 5),
-(9, 'Quem descobriu o Brasil foi Pedro Alvares Ca...', 1, 3),
-(10, 'Na frase: "Joazinho tem um posto" Jo√£ozinho √© um sujeito de que tipo?', 3, 4);
+CREATE TABLE `opcoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) NOT NULL,
+  `right_answer` tinyint(1) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `opcoes_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questoes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+#
+# Structure for the `usuarios` table : 
+#
 
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
+CREATE TABLE `usuarios` (
   `nome` varchar(50) NOT NULL,
   `sobrenome` varchar(50) NOT NULL,
   `usuario` varchar(50) NOT NULL,
@@ -257,28 +124,145 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   PRIMARY KEY (`usuario`,`senha`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `usuarios`
---
+#
+# Data for the `estatisticas` table  (LIMIT 0,500)
+#
 
-INSERT INTO `usuarios` (`nome`, `sobrenome`, `usuario`, `email`, `matricula`, `senha`, `nivel`) VALUES
-('abc', 'abc', 'abc', 'abc@abc.com', '123', '123', 1),
-('alice', 'asd', 'alicetotosa', 'asda', 'asd', 'e8d95a51f3af4a3b134bf6bb680a213a', 2),
-('Alysson', 'Moreira', 'alyssonmoreira', 'alyssonyopacom', '123456', 'senha', 8),
-('aasd', 'asd', 'asdf', 'asdfasdfcom', '123456', 'senha', 1),
-('123', '123', 'hugo', 'diniz', 'aaa', 'aaa@aaa.com', 1),
-('ozeias', 'dalmon', 'ozeias', 'wololologmailcom', '12345', 'e8d95a51f3af4a3b134bf6bb680a213a', 10);
+INSERT INTO `estatisticas` (`user`, `acertos_geral`, `total_questoes`, `total_mat`, `acertos_mat`, `total_geo`, `acertos_geo`, `total_hist`, `acertos_hist`, `total_port`, `acertos_port`, `total_quim`, `acertos_quim`, `total_ing`, `acertos_ing`, `total_esp`, `acertos_esp`, `total_soc`, `acertos_soc`, `total_fil`, `acertos_fil`, `total_fis`, `acertos_fis`, `total_bio`, `acertos_bio`, `jogos_realizados`) VALUES 
+  ('alyssonmoreira',105,105,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+  ('asdf',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+  ('alicetotosa',16,20,7,5,1,1,4,3,7,6,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0),
+  ('ozeias',149,160,57,53,6,6,45,41,32,30,20,19,0,0,0,0,0,0,0,0,0,0,0,0,12);
 
---
--- Restri√ß√µes para as tabelas dumpadas
---
+COMMIT;
 
---
--- Restri√ß√µes para a tabela `opcoes`
---
-ALTER TABLE `opcoes`
-  ADD CONSTRAINT `opcoes_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questoes` (`id`);
+#
+# Data for the `frases` table  (LIMIT 0,500)
+#
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `frases` (`id`, `content`, `reacao`) VALUES 
+  (1,'Ol·, aprendiz! Vamos construir o nosso foguete?',1),
+  (2,'VocÍ È um Ûtimo aprendiz.',2),
+  (3,'Sabia que a NASA j· tem 100 anos?',3),
+  (4,'Obrigada por estar me ajudando, aprendiz!',1),
+  (5,'VocÍ sabia que eu sou formado em Engenharia Mec‚nica?',5),
+  (6,'Vamos estudar!',1),
+  (7,'O seu desempenho est· Ûtimo!',2),
+  (8,'Continue estudando.',3);
+
+COMMIT;
+
+#
+# Data for the `materias` table  (LIMIT 0,500)
+#
+
+INSERT INTO `materias` (`id`, `nome`) VALUES 
+  (1,'Matem·tica'),
+  (2,'Geografia'),
+  (3,'HistÛria'),
+  (4,'PortuguÍs'),
+  (5,'QuÌmica'),
+  (6,'InglÍs'),
+  (7,'Espanhol'),
+  (8,'Sociologia'),
+  (9,'Filosofia'),
+  (11,'FÌsica'),
+  (12,'Biologia');
+
+COMMIT;
+
+#
+# Data for the `nivel` table  (LIMIT 0,500)
+#
+
+INSERT INTO `nivel` (`idNivel`, `nivel`) VALUES 
+  (1,'F·cil'),
+  (2,'MÈdio'),
+  (3,'DifÌcil');
+
+COMMIT;
+
+#
+# Data for the `questoes` table  (LIMIT 0,500)
+#
+
+INSERT INTO `questoes` (`id`, `titulo`, `idNivel`, `id_mate`) VALUES 
+  (1,'Quanto È 2 + 2?',1,1),
+  (2,'X + 2 = 4. Qual o valor de X? ',3,1),
+  (3,'5+5 = ?',2,1),
+  (4,'Como Tiradentes foi morto?',1,3),
+  (5,'Qual desses estados n„o faz parte do nordeste brasileiro?',2,2),
+  (6,'Qual o grau de relev‚ncia de oraÁ„o subordinada para a sua vida?',1,4),
+  (7,'Qual o n˙mero atÙmico do HidrogÍnio?',2,5),
+  (8,'Qual o n˙mero atÙmico do HÈlio?',2,5),
+  (9,'Quem descobriu o Brasil foi Pedro Alvares Ca...',1,3),
+  (10,'Na frase: \"Joazinho tem um posto\" Jo„ozinho È um sujeito de que tipo?',3,4);
+
+COMMIT;
+
+#
+# Data for the `opcoes` table  (LIMIT 0,500)
+#
+
+INSERT INTO `opcoes` (`id`, `content`, `right_answer`, `question_id`) VALUES 
+  (1,'5',0,1),
+  (2,'4',1,1),
+  (3,'3',0,1),
+  (4,'2',0,1),
+  (5,'1',0,2),
+  (6,'2',1,2),
+  (7,'3',0,2),
+  (8,'4',0,2),
+  (9,'8',0,3),
+  (10,'9',0,3),
+  (11,'10',1,3),
+  (12,'11',0,3),
+  (13,'Enforcado',1,4),
+  (14,'Com um tiro no cocurutu',0,4),
+  (15,'Negaram ·gua pra ele',0,4),
+  (16,'Ele se cansou de viver',0,4),
+  (17,'Cear·',0,5),
+  (18,'Maranh„o',0,5),
+  (19,'Pernambuco',0,5),
+  (20,'Bahia',0,5),
+  (21,'Quebec',1,5),
+  (22,'Enorme',0,6),
+  (23,'Razoavelmente Importante',0,6),
+  (24,'Importante',0,6),
+  (25,'N„o t„o Importante',0,6),
+  (26,'Irrelevante',1,6),
+  (29,'1',1,7),
+  (30,'2',0,7),
+  (31,'3',0,7),
+  (32,'4',0,7),
+  (33,'1',0,8),
+  (34,'2',1,8),
+  (35,'3',0,8),
+  (36,'4',0,8),
+  (37,'bral',1,9),
+  (38,'brel',0,9),
+  (39,'bril',0,9),
+  (40,'brol',0,9),
+  (41,'brul',0,9),
+  (42,'Composto',0,10),
+  (43,'Com posto',1,10),
+  (44,'Simples',0,10),
+  (45,'N„o sei',0,10),
+  (46,'Pergunta la no Posto Ipiranga',0,10);
+
+COMMIT;
+
+#
+# Data for the `usuarios` table  (LIMIT 0,500)
+#
+
+INSERT INTO `usuarios` (`nome`, `sobrenome`, `usuario`, `email`, `matricula`, `senha`, `nivel`) VALUES 
+  ('abc','abc','abc','abc@abc.com','123','123',1),
+  ('alice','asd','alicetotosa','asda','asd','e8d95a51f3af4a3b134bf6bb680a213a',2),
+  ('Alysson','Moreira','alyssonmoreira','alyssonyopacom','123456','senha',8),
+  ('aasd','asd','asdf','asdfasdfcom','123456','senha',1),
+  ('123','123','hugo','diniz','aaa','aaa@aaa.com',1),
+  ('ozeias','dalmon','ozeias','wololologmailcom','12345','e8d95a51f3af4a3b134bf6bb680a213a',10);
+
+COMMIT;
+
