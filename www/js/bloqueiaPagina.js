2172c3973aae1d3b823c0d94177ba07acf1f0028
Hugo <hhldiniz@gmail.com>
2015-06-22 11:42:59 -0300

    colocado o favicon via script para carrergar em todas as páginas dinamicamente, evitando trabalho manual

--- www/js/bloqueiaPagina.js
+++ www/js/bloqueiaPagina.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
 	{
 		$(location).attr("href","http://localhost/E2P/www/bloqueio.html");
 	}
+$("head").append("<link rel='icon' href='imagens/favicon.ico' type='image/x-icon'>"); //isso não deveria estar aqui, foi apenas para economizar trabalho de colocar essa linha em cada um dos arquivos. Essa linha apenas adiciona o favicon em todas as página
 });
